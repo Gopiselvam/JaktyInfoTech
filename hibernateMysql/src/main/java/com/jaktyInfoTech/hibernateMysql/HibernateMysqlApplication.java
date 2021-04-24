9cbf61dd3878a8879145744d99f603be8bfd22d3
Gopiselvam <gopiselvam96@gmail.com>
2021-04-24 21:25:42 +0530

    21APR2021 updated @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)

--- hibernateMysql/src/main/java/com/jaktyInfoTech/hibernateMysql/HibernateMysqlApplication.java
+++ hibernateMysql/src/main/java/com/jaktyInfoTech/hibernateMysql/HibernateMysqlApplication.java
@@ -3,7 +3,7 @@ package com.jaktyInfoTech.hibernateMysql;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class HibernateMysqlApplication {
 
 	public static void main(String[] args) {
